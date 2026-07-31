cf081aa799bc0a27f84d0649289ea4e8ccecd335
RANJITH K <ranjithk042006@gmail.com>
2026-07-31 06:14:30 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (25.19%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function(n) {
+    
+    if(n%2==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
